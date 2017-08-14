d6cd77e0270361f15c54dfa24c1bfd0e67130415
Norcross <andrew.norcross@gmail.com>
2017-08-14 10:21:16 -0400

    adding GH link to footer

--- includes/footer.php
+++ includes/footer.php
@@ -1,3 +1,3 @@
 <footer class="footer">
-	<p>&copy; <?php echo date( 'Y' ); ?> - built by <a href="http://andrewnorcross.com">Norcross</a></p>
+	<p>&copy; <?php echo date( 'Y' ); ?> | built by <a href="http://andrewnorcross.com">Norcross</a> | <a href="https://github.com/norcross/instagram-photo-url/">View on GitHub</a></p>
 </footer>
